082248dbf739b5f9b6ae218a90c5144ca52de306
kaoru <>
2021-09-11 06:18:09 +0900

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeaderBefore = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderAfter = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonRename = new System.Windows.Forms.Button();
+            this.textBoxDateFormat = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.AllowDrop = true;
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderBefore,
+            this.columnHeaderAfter});
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(776, 372);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listView1_DragDrop);
+            this.listView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listView1_DragEnter);
+            // 
+            // columnHeaderBefore
+            // 
+            this.columnHeaderBefore.Tag = "before";
+            this.columnHeaderBefore.Text = "元ファイル名";
+            this.columnHeaderBefore.Width = 369;
+            // 
+            // columnHeaderAfter
+            // 
+            this.columnHeaderAfter.Tag = "after";
+            this.columnHeaderAfter.Text = "新ファイル名";
+            this.columnHeaderAfter.Width = 393;
+            // 
+            // buttonRename
+            // 
+            this.buttonRename.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRename.Location = new System.Drawing.Point(543, 392);
+            this.buttonRename.Name = "buttonRename";
+            this.buttonRename.Size = new System.Drawing.Size(245, 46);
+            this.buttonRename.TabIndex = 1;
+            this.buttonRename.Text = "リネーム";
+            this.buttonRename.UseVisualStyleBackColor = true;
+            this.buttonRename.Click += new System.EventHandler(this.buttonRename_Click);
+            // 
+            // textBoxDateFormat
+            // 
+            this.textBoxDateFormat.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBoxDateFormat.Location = new System.Drawing.Point(12, 419);
+            this.textBoxDateFormat.Name = "textBoxDateFormat";
+            this.textBoxDateFormat.Size = new System.Drawing.Size(309, 19);
+            this.textBoxDateFormat.TabIndex = 2;
+            this.textBoxDateFormat.Text = "yyyy-MM-dd-HH-mm-ss";
+            this.textBoxDateFormat.TextChanged += new System.EventHandler(this.textBoxDateFormat_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxDateFormat);
+            this.Controls.Add(this.buttonRename);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Unixtimeファイル名変換器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeaderBefore;
+        private System.Windows.Forms.ColumnHeader columnHeaderAfter;
+        private System.Windows.Forms.Button buttonRename;
+        private System.Windows.Forms.TextBox textBoxDateFormat;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_DragEnter(object sender, DragEventArgs e)
+        {
+            //コントロール内にドラッグされたとき実行される
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                //ドラッグされたデータ形式を調べ、ファイルのときはコピーとする
+                e.Effect = DragDropEffects.Copy;
+            else
+                //ファイル以外は受け付けない
+                e.Effect = DragDropEffects.None;
+        }
+
+        private string unixtimePathToDatePath (string unixtimePath)
+        {
+            string filename = Path.GetFileName(unixtimePath);
+            string dir = Path.GetDirectoryName(unixtimePath);
+
+            var segments = new List<string>();
+            segments.AddRange(filename.Split('.'));
+            string ext = segments[segments.Count - 1];
+            segments.RemoveAt(segments.Count - 1);
+
+
+            string[] new_src;
+            new_src = segments.ToArray();
+            string basename = String.Join(".", new_src).Substring(0, 10);
+
+            if (long.TryParse(basename, out long result))
+            {
+                DateTime datetime = DateTimeOffset.FromUnixTimeSeconds(result).LocalDateTime;
+                string ymdhis = datetime.ToString(textBoxDateFormat.Text);
+
+                return dir + "\\" + ymdhis + "." + ext;
+            }
+
+            return "";
+        }
+
+        private void listView1_DragDrop(object sender, DragEventArgs e)
+        {
+            //コントロール内にドロップされたとき実行される
+            //ドロップされたすべてのファイル名を取得する
+            string[] paths = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            //ListBoxに追加する
+            foreach (string path in paths)
+            {
+                Boolean isExists = false;
+
+                foreach (ListViewItem item in this.listView1.Items)
+                {
+                    if (item.Text == path)
+                    {
+                        isExists = true;
+                        break;
+                    }
+                }
+
+                if (!isExists)
+                {
+                    string datePath = this.unixtimePathToDatePath(path);
+
+                    if (datePath != "")
+                    {
+                        ListViewItem item = new ListViewItem();
+                        item.Text = path;                    // 1列目に表示するテキスト
+                        item.ImageIndex = 0;                 // イメージのインデックス番号
+                        item.SubItems.Add(datePath);     // 2列目に表示するテキスト
+
+                        this.listView1.Items.Add(item);
+                    }
+                }
+            }
+        }
+
+        private void buttonRename_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in this.listView1.Items)
+            {
+                if (!File.Exists(item.SubItems[1].Text))
+                {
+                    File.Move(item.Text, item.SubItems[1].Text);
+                }
+            }
+
+            this.listView1.Items.Clear();
+        }
+
+        private void textBoxDateFormat_TextChanged(object sender, EventArgs e)
+        {
+            if (this.textBoxDateFormat.Text == "")
+            {
+                return;
+            }
+
+            foreach (ListViewItem item in this.listView1.Items)
+            {
+                item.SubItems[1].Text = this.unixtimePathToDatePath(item.Text);
+            }
+
+        }
+    }
+}
